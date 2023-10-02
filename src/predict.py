af544d0667ff541e8eb06d7f834e4f4011294059
Youki Nagashima <youki.nagashima@gmail.com>
2023-10-02 17:25:25 +0900

    [fix] metrics.csvのpathを誤って修正したのを元に戻した

--- src/predict.py
+++ src/predict.py
@@ -487,7 +487,7 @@ def main():
 
     csv_path = os.path.join(
         result_path,
-        "tab_graph",
+        f"{mode}",
         trained_model + f"_epoch{use_model_epoch}",
         "metrics.csv",
     )
