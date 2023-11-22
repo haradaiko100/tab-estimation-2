be1e693ed1e6ec20ee20f0d84dbbc716232e481e
Youki Nagashima <youki.nagashima@gmail.com>
2023-11-22 13:06:36 +0900

    [add] 特定の弦とフレットの組み合わせに対して異弦同音となる組み合わせを算出できるようにした

--- src/get_problem_file.py
+++ src/get_problem_file.py
@@ -2,52 +2,57 @@ import os
 import argparse
 import numpy as np
 from predict import tab2pitch
+from src.const.const import GUITAR_SOUND_MATRICS
 
 
-
-def get_same_sound_on_different_strings(tab,pred_tab):
-
+# 関数の名前ひどいから後で変える
+def get_issue_data(tab, pred_tab):
     # まず正解のタブ譜と予測されたタブ譜において、正解となる音は排除する必要がある
-    test_tab = np.array(
-                [
-                    [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                    ],
-                    [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                    ],
-                    [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                    ],
-                    [
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-                    ],
-                ]
-            )
-    
-    pitch_result = tab2pitch(test_tab)
-    print(pitch_result)
+    # 排除するのは教師データの方だけで良い事に注意
 
+    pass
+
+
+def get_finger_positions_on_specific_sound(fret, string):
+    same_sound_different_strings = {}
+
+    if fret >= len(GUITAR_SOUND_MATRICS[string]):
+        return same_sound_different_strings
+
+    target_value = GUITAR_SOUND_MATRICS[string][fret]
+
+    for row_index, row in enumerate(GUITAR_SOUND_MATRICS):
+        for col_index, value in enumerate(row):
+            if value == target_value:
+                same_sound_different_strings[row_index] = col_index
+
+    return same_sound_different_strings
+
+
+def find_value_positions(matrix, target_value):
+    positions = []
+
+    for row_index, row in enumerate(matrix):
+        for col_index, value in enumerate(row):
+            if value == target_value:
+                positions.append((row_index, col_index))
+
+    return positions
+
+
+# 例として、次のような二次元配列を使ってみます
+# example_matrix = [
+#     [1, 2, 3, 4],
+#     [5, 6, 7, 8],
+#     [9, 2, 11, 12],
+#     [13, 14, 2, 16]
+# ]
+
+# target_value = 2
+
+# result = find_value_positions(example_matrix, target_value)
+# print(f"Target Value: {target_value}")
+# print("Positions:", result)
 
 
 def get_problem_files_name():
@@ -73,9 +78,7 @@ def get_problem_files_name():
         "result", "tab", f"{trained_model}_epoch{use_model_epoch}", "npz"
     )
 
-    
-
 
 if __name__ == "__main__":
     # get_problem_files_name()
-    get_same_sound_on_different_strings(1,2)
+    get_finger_positions_on_specific_sound(1, 2)
